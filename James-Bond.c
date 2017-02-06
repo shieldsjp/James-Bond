52a3d0f87c25b7e4da0e5f6a7a7139f6ef92def8
John Shields <shieldsjp@outlook.com>
2017-02-06 16:33:29 -0500

    Add files via upload

---
+++ James-Bond.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main (void)
+{
+ int i;
+ char  array_values[30] = "QJWAMMDECS$ EBDONNEDXYZ";
+
+ for ( i=1 ; i < 21; i = i + 2)
+       printf("%c", array_values[i]);
+
+}
